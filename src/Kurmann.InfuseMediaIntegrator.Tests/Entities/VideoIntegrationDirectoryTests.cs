38bbec8db29d475f798f659d083abdae0b57ccd8
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-17 14:56:25 +0000

    Refactor VideoIntegrationDirectory to handle not supported files #6

--- src/Kurmann.InfuseMediaIntegrator.Tests/Entities/VideoIntegrationDirectoryTests.cs
+++ src/Kurmann.InfuseMediaIntegrator.Tests/Entities/VideoIntegrationDirectoryTests.cs
@@ -5,7 +5,7 @@ namespace Kurmann.InfuseMediaIntegrator.Tests.Entities;
 [TestClass]
 public class VideoIntegrationDirectoryTests
 {
-    private const string InputDirectoryPath = "../../Data/Input";
+    private const string InputDirectoryPath = "Data/Input";
 
     /// <summary>
     /// Testet die Methode <see cref="VideoIntegrationDirectory.Create(string)"/>.
@@ -24,6 +24,23 @@ public class VideoIntegrationDirectoryTests
         Assert.IsTrue(result.IsSuccess);
         Assert.AreEqual(1, result.Value.Mpeg4VideoFiles.Count); // Hier sollte nur die MPEG4-Datei enthalten sein
         Assert.AreEqual(1, result.Value.QuickTimeVideoFiles.Count); // Hier sollte nur die QuickTime-Datei enthalten sein
+    }
+
+    /// <summary>
+    /// Testet, ob die Test-Datei mit TXT-Dateiendung als nicht unterstützte Datei erkannt wird.
+    /// </summary>
+    [TestMethod]
+    public void Create_ShouldReturnSuccess_WhenDirectoryContainsNotSupportedFile()
+    {
+        // Arrange
+        var directoryPath = InputDirectoryPath;
+        
+        // Act
+        var result = VideoIntegrationDirectory.Create(directoryPath);
+        
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.AreEqual(1, result.Value.NotSupportedFiles.Count); // Hier sollte nur die TXT-Datei enthalten sein
     }
     
     [TestMethod]
--- src/Kurmann.InfuseMediaIntegrator/Entities/VideoIntegrationDirectory.cs
+++ src/Kurmann.InfuseMediaIntegrator/Entities/VideoIntegrationDirectory.cs
@@ -5,10 +5,13 @@ namespace Kurmann.InfuseMediaIntegrator.Entities;
 /// <summary>
 /// Repräsentiert eine Sammlung von MPEG4-Video-Dateien mit eingebetteten Metadaten.
 /// </summary>
-public class VideoIntegrationDirectory(IEnumerable<Mpeg4Video> mpeg4VideoFiles, IEnumerable<QuickTimeVideo> quickTimeVideoFiles)
+public class VideoIntegrationDirectory(IEnumerable<Mpeg4Video> mpeg4VideoFiles,
+                                       IEnumerable<QuickTimeVideo> quickTimeVideoFiles,
+                                       IEnumerable<NotSupportedFile> notSupportedFiles)
 {
     public IReadOnlyList<Mpeg4Video> Mpeg4VideoFiles { get; } = mpeg4VideoFiles.ToList();
     public IReadOnlyList<QuickTimeVideo> QuickTimeVideoFiles { get; } = quickTimeVideoFiles.ToList();
+    public IReadOnlyList<NotSupportedFile> NotSupportedFiles { get; } = notSupportedFiles.ToList();
 
     public static Result<VideoIntegrationDirectory> Create(string directoryPath)
     {
@@ -30,31 +33,49 @@ public class VideoIntegrationDirectory(IEnumerable<Mpeg4Video> mpeg4VideoFiles, 
             // Filtere die MPEG4-Dateien
             var mpeg4VideoFiles = new List<Mpeg4Video>();
             var quickTimeVideoFiles = new List<QuickTimeVideo>();
+            var notSupportedFiles = new List<NotSupportedFile>();
             foreach (var file in files)
             {
                 // Erstelle ein Mpeg4VideoFileWithEmbeddedMetadata-Objekt
                 var mpeg4VideoFile = Mpeg4Video.Create(file.FullName);
 
-                // Prüfe, ob das Objekt erstellt werden konnte, falls nicht, ignoriere die Datei
+                // Prüfe, ob das Objekt erstellt werden konnte, und füge es der Liste hinzu
                 if (mpeg4VideoFile.IsFailure)
-                    continue;
+                {
+                    if (mpeg4VideoFile.IsSuccess)
+                    {
 
-                // Füge das Objekt der Liste hinzu
-                mpeg4VideoFiles.Add(mpeg4VideoFile.Value);
-
-                // Erstelle ein QuickTimeVideoFileWithEmbeddedMetadata-Objekt
+                        mpeg4VideoFiles.Add(mpeg4VideoFile.Value);     
+                    }
+                
+                // Erstelle ein QuickTimeVideo-Objekt
                 var quickTimeVideoFile = QuickTimeVideo.Create(file.FullName);
 
-                // Prüfe, ob das Objekt erstellt werden konnte, falls nicht, ignoriere die Datei
+                // Prüfe, ob das Objekt erstellt werden konnte, und füge es der Liste hinzu
                 if (quickTimeVideoFile.IsFailure)
-                    continue;
+                {
+                    if (quickTimeVideoFile.IsSuccess)
+                        quickTimeVideoFiles.Add(quickTimeVideoFile.Value);
+                }
 
-                // Füge das Objekt der Liste hinzu
-                quickTimeVideoFiles.Add(quickTimeVideoFile.Value);
+                // Erstelle ein NotSupportedFile-Objekt
+                var notSupportedFile = NotSupportedFile.Create(file.FullName, $"File '{file.Name}' is not supported.");
+
+                // Prüfe, ob das Objekt erstellt werden konnte, und füge es der Liste hinzu
+                if (notSupportedFile.IsFailure)
+                {
+                    if (notSupportedFile.IsSuccess)
+                        notSupportedFiles.Add(notSupportedFile.Value);
+                }
+                
+                // Wenn das NotSupportedFile-Objekt nicht erstellt werden konnte, gib eine Fehlermeldung aus
+                if (notSupportedFile.IsFailure)
+                    return Result.Failure<VideoIntegrationDirectory>($"Error on reading file info: {notSupportedFile.Error}");
+
             }
 
             // Rückgabe des VideoIntegrationDirectory-Objekts
-            return Result.Success(new VideoIntegrationDirectory(mpeg4VideoFiles, quickTimeVideoFiles));
+            return Result.Success(new VideoIntegrationDirectory(mpeg4VideoFiles, quickTimeVideoFiles, notSupportedFiles));
         }
         catch (Exception e)
         {
