7192a703be7344431139eed79ba1ecb8e439b1ae
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-17 09:01:49 +0000

    can run test with test runner #5

--- src/Kurmann.InfuseMediaIntegrator.Tests/Entities/Mpeg4VideoInputFilesTest.cs
+++ src/Kurmann.InfuseMediaIntegrator.Tests/Entities/Mpeg4VideoInputFilesTest.cs
@@ -5,32 +5,32 @@ namespace Kurmann.InfuseMediaIntegrator.Tests.Entities;
 [TestClass]
 public class Mpeg4VideoInputFilesTests
 {
+    private const string InputDirectoryPath = "../../../tests/Data/Input";
 
     [TestMethod]
     public void Create_ShouldReturnSuccess_WhenDirectoryExists()
     {
         // Arrange
-        var directoryPath = InputDirectoryPath;
+        var directoryPath = Path.Combine(InputDirectoryPath, "Zwillinge Testvideo.m4v");
         
         // Act
         var result = Mpeg4VideoInputFiles.Create(directoryPath);
         
         // Assert
-        Assert.True(result.IsSuccess);
-        Assert.NotNull(result.Value);
+        Assert.IsTrue(result.IsSuccess);
     }
     
     [TestMethod]
     public void Create_ShouldReturnFailure_WhenDirectoryNotFound()
     {
         // Arrange
-        var directoryPath = InputDirectoryPath;
+        var directoryPath = Path.Combine("../not/existing", "Zwillinge Testvideo.m4v");
         
         // Act
         var result = Mpeg4VideoInputFiles.Create(directoryPath);
         
         // Assert
-        Assert.True(result.IsFailure);
-        Assert.Equal("Directory not found.", result.Error);
+        Assert.AreEqual("Directory not found.", result.Error);
+        Assert.IsTrue(result.IsFailure);
     }
 }
