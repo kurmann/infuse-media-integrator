1bb8041ec1b10d9c0b27ca6e838d72a88a68e6a3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-18 18:50:37 +0000

    refactor to video file type (not using file info) #9

--- src/ConsoleApp/Entities/SupportedVideoFileType.cs
+++ src/ConsoleApp/Entities/SupportedVideoFileType.cs
@@ -3,26 +3,18 @@ using CSharpFunctionalExtensions;
 namespace Kurmann.InfuseMediaIntegrator;
 
 /// <summary>
-/// Repräsentiert eine unterstützte Video-Datei.
-/// Unterstützte Dateitypen sind QuickTime und MPEG4.
-/// Erkennung anhand der Dateiendung.
+/// Nimmt eine Datei entgegen und prüft, ob es sich um eine unterstützte Video-Datei handelt und bestimmt den Video-Datentyp.
 /// </summary>
-public class SupportedVideoFile
+public class SupportedVideoFileType
 {
     private static readonly string[] quickTimeFileExtensions = [".mov", ".qt"];
     private static readonly string[] mpeg4FileExtensions = [".mp4", ".m4v"];
 
     public VideoFileType Type { get;}
 
-    public FileInfo FileInfo { get; }
+    private SupportedVideoFileType(VideoFileType type) => Type = type;
 
-    private SupportedVideoFile(FileInfo fileInfo, VideoFileType type)
-    {
-        FileInfo = fileInfo;
-        Type = type;
-    }
-
-    public static Result<SupportedVideoFile> Create(string path)
+    public static Result<SupportedVideoFileType> Create(string path)
     {
         try
         {
@@ -31,24 +23,25 @@ public class SupportedVideoFile
 
             // Prüfe, ob die Datei existiert
             if (!fileInfo.Exists)
-                return Result.Failure<SupportedVideoFile>("File not found.");
+                return Result.Failure<SupportedVideoFileType>("File not found.");
 
             // Bestimme den Dateityp anhand der Dateiendung und berücksichtige dabei die Groß-/Kleinschreibung nicht
             if (quickTimeFileExtensions.Contains(fileInfo.Extension, StringComparer.OrdinalIgnoreCase))
-                return new SupportedVideoFile(fileInfo, VideoFileType.QuickTime);
+                return new SupportedVideoFileType(VideoFileType.QuickTime);
             if (mpeg4FileExtensions.Contains(fileInfo.Extension, StringComparer.OrdinalIgnoreCase))
-                return new SupportedVideoFile(fileInfo, VideoFileType.Mpeg4);
-            return Result.Failure<SupportedVideoFile>("File is not a supported video file.");
+                return new SupportedVideoFileType(VideoFileType.Mpeg4);
+            return new SupportedVideoFileType(VideoFileType.NotSupportedFile);
         }
         catch (Exception e)
         {
-            return Result.Failure<SupportedVideoFile>($"Error on reading file info: {e.Message}");
+            return Result.Failure<SupportedVideoFileType>($"Error on reading file info: {e.Message}");
         }
     }
 }
 
 public enum VideoFileType
 {
+    NotSupportedFile,
     QuickTime,
     Mpeg4
 }
