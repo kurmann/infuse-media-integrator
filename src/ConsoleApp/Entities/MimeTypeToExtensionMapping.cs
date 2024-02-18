d02e04acf2d1552964c7ed2f720db5d890a42c3c
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-18 21:45:57 +0000

    Add MimeTypeToExtensionMapping class for mapping MIME types to file extensions #11

---
+++ src/ConsoleApp/Entities/MimeTypeToExtensionMapping.cs
@@ -0,0 +1,34 @@
+using CSharpFunctionalExtensions;
+
+namespace Kurmann.InfuseMediaIntegrator.Entities;
+
+public class MimeTypeToExtensionMapping
+{
+    public string MimeType { get; }
+    public string Extension { get; }
+
+    private MimeTypeToExtensionMapping(string mimeType, string extension)
+    {
+        MimeType = mimeType;
+        Extension = extension;
+    }
+
+    public static Result<MimeTypeToExtensionMapping> Create(string mimeType)
+    {
+        if (string.IsNullOrWhiteSpace(mimeType))
+            return Result.Failure<MimeTypeToExtensionMapping>("MIME-Typ ist nicht definiert.");
+
+        string extension = mimeType.Split('/')[1];
+
+        if (extension == "jpeg")
+            extension = "jpg";
+        if (extension == "tiff")
+            extension = "tif";
+        if (extension == "x-ms-bmp")
+            extension = "bmp";
+        if (extension == "x-icon")
+            extension = "ico";
+
+        return new MimeTypeToExtensionMapping(mimeType, extension);
+    }
+}
