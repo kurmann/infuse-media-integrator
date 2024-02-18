9042d459c9f42c7c0ec8965e4077f9faeb0428f3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-18 20:57:27 +0000

    Delete InfuseSupportedMpeg4Metadata class 11

--- src/ConsoleApp/Entities/InfuseSupportedMpeg4Metadata.cs
+++
@@ -1,9 +0,0 @@
-namespace Kurmann.InfuseMediaIntegrator;
-
-/// <summary>
-/// Repräsentiert die Metadaten, die von Infuse in einer MPEG4-Datei unterstützt werden.
-/// </summary>
-public class InfuseSupportedMpeg4Metadata
-{
-
-}
