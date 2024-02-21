3c63ed9463814e95a1226303bc4819c5770c2b0a
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-21 19:24:21 +0000

    Add MoveFilesToMediaLibraryCommand class #14

---
+++ src/ConsoleApp/Commands/MoveFilesToMediaLibraryCommand.cs
@@ -0,0 +1,13 @@
+using CSharpFunctionalExtensions;
+using Kurmann.InfuseMediaIntegrator.Commands;
+
+namespace Kurmann.InfuseMediaIntegrator.Tests.Entities;
+
+public class MoveFilesToMediaLibraryCommand : ICommand
+{
+
+    public Result Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
