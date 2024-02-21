3d88c2e5b80e0b5fe73ed06130c5218b0d493cc1
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-21 16:26:17 +0000

    Add FileMappingInfoTests class with test methods for creating file mapping info #13

---
+++ src/Tests/Entities/FileMappingInfoTests.cs
@@ -0,0 +1,126 @@
+using Kurmann.InfuseMediaIntegrator.Entities;
+
+namespace Kurmann.InfuseMediaIntegrator.Tests.Entities;
+
+[TestClass]
+public class FileMappingInfoTests
+{
+    [TestMethod]
+    public void Create_ShouldReturnFailure_WhenCategoryIsNullOrWhitespace()
+    {
+        // Arrange
+        var category = string.Empty;
+        var fileName = "2024-21-03 Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        StringAssert.Contains(result.Error, "Category cannot be null or whitespace.");
+        Assert.IsTrue(result.IsFailure);
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnFailure_WhenFileNameDoesNotHaveExtension()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "2024-21-03 Ausflug nach Willisau";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        StringAssert.Contains(result.Error, "File name does not match the expected format");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnFailure_WhenFileNameDoesNotHaveYear()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        StringAssert.Contains(result.Error, "File name does not match the expected format");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnFailure_WhenFileNameHaveNoWhitespaceBetweenDateAndTitle()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "2024-21-03Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        StringAssert.Contains(result.Error, "File name does not match the expected format");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnSuccess_WhenFileNameHaveYearAndTitle()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "2024 Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.AreEqual(2024, result.Value.Year);
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnFailure_WhenYearIsOlderThan1900()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "1899-21-03 Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        StringAssert.Contains(result.Error, "File name does not match the expected format");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnSuccess_WhenFileNameHasYearWithMonthAndTitle()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "2024-21 Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.AreEqual(2024, result.Value.Year);
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnSuccess_WhenFileNameHasIsoDateAndTitle()
+    {
+        // Arrange
+        var category = "Familie";
+        var fileName = "2024-21-03 Ausflug nach Willisau.m4v";
+        
+        // Act
+        var result = FileMappingInfo.Create(category, fileName);
+        
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.AreEqual(category, result.Value.Category);
+        Assert.AreEqual(2024, result.Value.Year);
+        Assert.AreEqual("Ausflug nach Willisau", result.Value.SortingTitle);
+        Assert.AreEqual("Familie/2024/Ausflug nach Willisau.m4v", result.Value.TargetPath);
+    }
+}
