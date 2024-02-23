475d4a1a730560aa0e62177224a57a5c96795390
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-23 14:44:36 +0000

    category supports implicit sub categories by slashes #15

--- src/ConsoleApp/Entities/CategoryInfo.cs
+++ src/ConsoleApp/Entities/CategoryInfo.cs
@@ -3,23 +3,29 @@ using CSharpFunctionalExtensions;
 namespace Kurmann.InfuseMediaIntegrator.Entities;
 
 /// <summary>
-/// Liest aus einem Wurzelverzeichnis (als Referenz) die Kategorien aus und speichert diese in einer Liste.
-/// Das erste Listen-Element ist die Hauptkatetorie (entspricht dem nächsten Verzeichnis unter dem Wurzelverzeichnis).
-/// Die weiteren Listen-Elemente sind die Unterkategorien (entspricht den weiteren Verzeichnissen unter dem Wurzelverzeichnis).
+/// Repräsentiert eine Kategorie-Information.
+/// Besteht aus unterscheidbaren Hauptkategorien, die jeweils Unterkategorien haben können.
 /// </summary>
 public class CategoryInfo
 {
-    private CategoryInfo(List<string> categories) => Categories = categories;
+    private CategoryInfo(HashSet<string> categories) => Categories = categories;
+    private CategoryInfo(string? category) => Categories = category != null ? [category] : [];
+    private CategoryInfo() => Categories = [];
 
     /// <summary>
-    /// Liste der Kategorien. Das erste Element ist die Hauptkategorie, die weiteren Elemente sind die Unterkategorien.
+    /// Liste der Kategorien. Eine Kategorie kann auch Unterkategorien haben, die durch einen Schrägstrich getrennt sind.
+    /// Beispiel: "Lyssach/Garten"
+    /// In diesem Fall ist "Lyssach" die Hauptkategorie und "Garten" die Unterkategorie.
+    /// Dieser Ansatz wird verwendet aufgrund der Einfachheit und der Tatsache, dass die Kategorien eine Verzeichnisstruktur nachahmen.
     /// </summary>
-    public List<string> Categories { get; }
+    public HashSet<string> Categories { get; }
 
     /// <summary>
     /// Erstellt eine neue Instanz der Klasse <see cref="CategoryInfo"/> aus einem Wurzelverzeichnis und einem Verzeichnis.
     /// Beispiel: RootPath = "Data/Input/Testcase 3", DirectoryPath = "Data/Input/Testcase 3/Lyssach/Garten"
-    /// Result: ["Lyssach", "Garten"]
+    /// Hinweis: Da eine Datei nur einer Verzeichnishierarchie zugehören kann, kann nur mit einer Verzeichnisstruktur nur
+    /// eine Kategorie ausgelesen werden. Etwaige Unterverzeichnisse werden als Unterkategorien interpretiert.
+    /// Beispiel: "Data/Input/Testcase 3/Lyssach/Garten" wird zu "Lyssach/Garten" wenn "Data/Input/Testcase 3" als Wurzelverzeichnis verwendet wird.
     /// </summary>
     /// <param name="rootPath"></param>
     /// <param name="directoryPath"></param>
@@ -43,17 +49,17 @@ public class CategoryInfo
         if (!directory.Exists)
             return Result.Failure<CategoryInfo>($"DirectoryPath '{directoryPath}' does not exist");
 
+        // Ermittle das relative Verzeichnis zum Wurzelverzeichnis
+        var relativeDirectory = directory.FullName.Substring(root.FullName.Length).Trim(Path.DirectorySeparatorChar);
+        
+        // Das relative Verzeichnis wird als Kategorie interpretiert (mit Unterkategorien getrennt durch Schrägstriche)
+        var category = relativeDirectory;
+
         // Wenn das Wurzelverzeichnis und das Verzeichnis das gleiche sind, dann ist die Kategorie leer
         if (root.FullName == directory.FullName)
-            return Result.Success(new CategoryInfo([]));
+            return new CategoryInfo();
 
-        // Erstelle das relative Verzeichnis, vom Wurzelverzeichnis aus gesehen.
-        var relativeDirectory = directory.FullName[(root.FullName.Length + 1)..];
-
-        // Erstelle die Kategorien aus dem relativen Verzeichnis.
-        var categories = relativeDirectory.Split(Path.DirectorySeparatorChar).ToList();
-
-        return Result.Success(new CategoryInfo(categories));
+        return new CategoryInfo(category);
     }
 
     // Erstellt eine neue Instanz der Klasse <see cref="CategoryInfo"/> aus einer Liste von Komma-separierten Kategorien.
@@ -62,41 +68,25 @@ public class CategoryInfo
         if (string.IsNullOrWhiteSpace(categories))
             return Result.Failure<CategoryInfo>("Categories is null or empty");
 
-        var categoriesList = categories.Split(',').Select(c => c.Trim()).ToList();
+        // Entferne Leerzeichen und trenne die Kategorien an den Kommas
+        var categoriesList = categories.Split(',').Select(c => c.Trim()).ToHashSet();
+
         return Result.Success(new CategoryInfo(categoriesList));
     }
-}
 
-/// <summary>
-/// Repräsentiert eine Kategorie.
-/// </summary>
-public class Category
-{
-    /// <summary>
-    /// Name der Kategorie.
-    /// </summary>
-    public string Name { get; }
+    public override string ToString() => string.Join(", ", Categories);
 
-    /// <summary>
-    /// Liste der Unterkategorien.
-    /// </summary>
-    public IReadOnlyList<Category> Subcategories { get; }
+    public static implicit operator string(CategoryInfo categoryInfo) => categoryInfo.ToString();
 
-    private Category(string name, List<Category> subcategories)
+    public Result AddCategory(string category)
     {
-        Name = name;
-        Subcategories = subcategories.AsReadOnly();
-    }
+        if (string.IsNullOrWhiteSpace(category))
+            return Result.Failure("Category is null or empty");
 
-    public static Result<Category> Create(string name, List<Category>? subcategories = null)
-    {
-        subcategories ??= [];
+        // Entferne die Leerzeichen und trenne sie anhand Kommas
+        var newCategories = category.Split(',').Select(c => c.Trim()).ToList();
 
-        if (string.IsNullOrWhiteSpace(name))
-        {
-            return Result.Failure<Category>("Category name cannot be empty.");
-        }
-
-        return Result.Success(new Category(name, subcategories));
+        Categories.UnionWith(newCategories);
+        return Result.Success();
     }
 }
