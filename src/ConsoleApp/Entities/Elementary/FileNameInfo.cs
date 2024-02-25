9a060ef9539cc7da138f7a4ae6b013b70defbdb3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-25 20:03:41 +0000

    introduced file name info #17

---
+++ src/ConsoleApp/Entities/Elementary/FileNameInfo.cs
@@ -0,0 +1,33 @@
+using CSharpFunctionalExtensions;
+
+namespace Kurmann.InfuseMediaIntegrator.Entities;
+
+/// <summary>
+/// Repräsentiert Informationen über einen Dateinamen, ohne direkt vom Dateisystem abhängig zu sein. Diese Klasse ist unveränderlich.
+/// </summary>
+public class FileNameInfo
+{
+    /// <summary>
+    /// Der Dateiname.
+    /// </summary>
+    public string Value { get; }
+
+    private FileNameInfo(string fullPath) => Value = Path.GetFileName(fullPath);
+
+    public static Result<FileNameInfo> Create(string fileName)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+        {
+            return Result.Failure<FileNameInfo>("File name is null or empty");
+        }
+
+        // Prüfe auf unzulässige Zeichen im Dateinamen
+        char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+        if (fileName.Any(c => invalidFileNameChars.Contains(c)))
+        {
+            return Result.Failure<FileNameInfo>("File name contains invalid characters: " + string.Join(", ", invalidFileNameChars));
+        }
+
+        return Result.Success(new FileNameInfo(fileName));
+    }
+}
