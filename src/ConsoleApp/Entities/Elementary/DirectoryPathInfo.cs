1df255d1c5ffd6c260bdc9ef1345666b9f529909
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-26 20:26:59 +0000

    created info for directories without file #17

---
+++ src/ConsoleApp/Entities/Elementary/DirectoryPathInfo.cs
@@ -0,0 +1,49 @@
+using CSharpFunctionalExtensions;
+
+namespace Kurmann.InfuseMediaIntegrator.Entities.Elementary
+{
+    /// <summary>
+    /// Repräsentiert Informationen über einen Verzeichnispfad, ohne direkt vom Dateisystem abhängig zu sein. Diese Klasse ist unveränderlich.
+    /// Unterscheidet sich von FilePathInfo, da es keine Informationen über den Dateinamen enthält.
+    /// </summary>
+    public class DirectoryPathInfo
+    {
+        /// <summary>
+        /// Der vollständige Pfad.
+        /// </summary>
+        public string Path { get; }
+
+        private DirectoryPathInfo(string path) => Path = path;
+
+        /// <summary>
+        /// Erstellt eine Instanz von DirectoryPathInfo, wenn der gegebene Pfad gültig ist.
+        /// </summary>
+        /// <param name="path">Der Pfad, der validiert und gespeichert werden soll.</param>
+        /// <returns>Ein Result-Objekt, das entweder eine DirectoryPathInfo-Instanz oder einen Fehler enthält.</returns>
+        public static Result<DirectoryPathInfo> Create(string? path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return Result.Failure<DirectoryPathInfo>("Path is null or empty");
+            }
+
+            // Entferne die Datei im Pfad, falls vorhanden
+            path = System.IO.Path.GetDirectoryName(path);
+
+            // Gib eine Fehlermeldung zurück, wenn der Pfad kein Verzeichnis ist
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return Result.Failure<DirectoryPathInfo>("Path is not a directory");
+            }
+
+            // Prüfe auf unzulässige Zeichen im Pfad
+            char[] invalidPathChars = System.IO.Path.GetInvalidPathChars();
+            if (path.Any(c => invalidPathChars.Contains(c)))
+            {
+                return Result.Failure<DirectoryPathInfo>("Path contains invalid characters: " + string.Join(", ", invalidPathChars));
+            }
+
+            return Result.Success(new DirectoryPathInfo(path));
+        }
+    }
+}
