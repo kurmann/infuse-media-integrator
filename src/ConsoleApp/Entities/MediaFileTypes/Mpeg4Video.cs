6256c0f78c25c0af3aaf87596ea7d0c05100279b
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-28 21:24:39 +0000

    moved to new namespace #17

--- src/ConsoleApp/Entities/MediaFileTypes/Mpeg4Video.cs
+++ src/ConsoleApp/Entities/MediaFileTypes/Mpeg4Video.cs
@@ -1,6 +1,6 @@
 using CSharpFunctionalExtensions;
 
-namespace Kurmann.InfuseMediaIntegrator.Entities;
+namespace Kurmann.InfuseMediaIntegrator.Entities.MediaFileTypes;
 
 /// <summary>
 /// Repräsentiert eine MPEG4-Video-Datei mit eingebetteten Metadaten.
