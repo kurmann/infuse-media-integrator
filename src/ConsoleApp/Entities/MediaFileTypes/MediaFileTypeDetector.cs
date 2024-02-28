de37b3ca791f7639996847c09c0ff6d9171a358b
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-28 21:52:18 +0000

    detector returns correct media file type #17

--- src/ConsoleApp/Entities/MediaFileTypes/MediaFileTypeDetector.cs
+++ src/ConsoleApp/Entities/MediaFileTypes/MediaFileTypeDetector.cs
@@ -1,41 +1,59 @@
 using CSharpFunctionalExtensions;
+using Kurmann.InfuseMediaIntegrator.Entities.Elementary;
 
 namespace Kurmann.InfuseMediaIntegrator.Entities.MediaFileTypes;
 
-
 public class MediaFileTypeDetector
 {
-    private static readonly string[] quickTimeFileExtensions = [".mov", ".qt"];
-    private static readonly string[] mpeg4FileExtensions = [".mp4", ".m4v"];
-    private static readonly string[] jpegFileExtensions = [".jpg", ".jpeg"];
 
-    public VideoFileType Type { get;}
-
-    private MediaFileTypeDetector(VideoFileType type) => Type = type;
-
-    public static Result<MediaFileTypeDetector> Create(string path)
+    public static Result<IMediaFileType> GetMediaFile(string path)
     {
         try
         {
-            // Erstelle ein FileInfo-Objekt
-            var fileInfo = new FileInfo(path);
+            // Erstelle ein FilePathInfo-Objekt
+            var filePath = FilePathInfo.Create(path);
+            if (filePath.IsFailure)
+                return Result.Failure<IMediaFileType>($"Error on reading file info: {filePath.Error}");
 
-            // Bestimme den Dateityp anhand der Dateiendung und berücksichtige dabei die Groß-/Kleinschreibung nicht
-            if (quickTimeFileExtensions.Contains(fileInfo.Extension, StringComparer.OrdinalIgnoreCase))
-                return new MediaFileTypeDetector(VideoFileType.QuickTime);
-            if (mpeg4FileExtensions.Contains(fileInfo.Extension, StringComparer.OrdinalIgnoreCase))
-                return new MediaFileTypeDetector(VideoFileType.Mpeg4);
-            if (jpegFileExtensions.Contains(fileInfo.Extension, StringComparer.OrdinalIgnoreCase))
-                return new MediaFileTypeDetector(VideoFileType.Jpeg);
-            return new MediaFileTypeDetector(VideoFileType.NotSupported);
+            // Lies die Dateiendung
+            var extension = Path.GetExtension(filePath.Value.FilePath).ToLowerInvariant();
+
+            // Gib das passende IMediaFileType-Objekt zurück
+            if (Mpeg4Video.FileExtensions.Contains(extension))
+            {
+                var mpeg4Video = Mpeg4Video.Create(path);
+                if (mpeg4Video.IsFailure)
+                    return Result.Failure<IMediaFileType>($"Error on reading file info: {mpeg4Video.Error}");
+                return mpeg4Video.Value;
+            }
+            if (QuickTimeVideo.FileExtensions.Contains(extension))
+            {
+                var quickTimeVideo = QuickTimeVideo.Create(path);
+                if (quickTimeVideo.IsFailure)
+                    return Result.Failure<IMediaFileType>($"Error on reading file info: {quickTimeVideo.Error}");
+                return quickTimeVideo.Value;
+            }
+            if (JpegImage.FileExtensions.Contains(extension))
+            {
+                var jpegImage = JpegImage.Create(path);
+                if (jpegImage.IsFailure)
+                    return Result.Failure<IMediaFileType>($"Error on reading file info: {jpegImage.Error}");
+                return jpegImage.Value;
+            }
+            var notSupportedFile = NotSupportedFile.Create(path, "File type not supported");
+            if (notSupportedFile.IsFailure)
+                return Result.Failure<IMediaFileType>($"Error on reading file info: {notSupportedFile.Error}");
+            return notSupportedFile.Value;
+
         }
         catch (Exception e)
         {
-            return Result.Failure<MediaFileTypeDetector>($"Error on reading file info: {e.Message}");
+            return Result.Failure<IMediaFileType>($"Error on reading file info: {e.Message}");
         }
     }
 }
 
+[Obsolete("Use IMediaFileType instead.")]
 public enum VideoFileType
 {
     NotSupported,
