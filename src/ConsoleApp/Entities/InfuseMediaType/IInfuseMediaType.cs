0e43b9cbbee081c328fad542988c7a2ce711f547
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-28 21:13:39 +0000

    defined infuse media type interface #17

---
+++ src/ConsoleApp/Entities/InfuseMediaType/IInfuseMediaType.cs
@@ -0,0 +1,8 @@
+using Kurmann.InfuseMediaIntegrator.Entities.Elementary;
+
+namespace Kurmann.InfuseMediaIntegrator.Entities.InfuseMediaType;
+
+public interface IInfuseMediaType
+{
+    FilePathInfo FilePath { get; }
+}
