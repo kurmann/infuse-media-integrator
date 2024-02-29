3bf2fff041f86481d29e9f01cc7182efb7fda871
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-29 13:56:19 +0000

    create lib organization info #17

--- src/ConsoleApp/Entities/MediaFileLibraryOrganizationInfo.cs
+++ src/ConsoleApp/Entities/MediaFileLibraryOrganizationInfo.cs
@@ -14,11 +14,6 @@ namespace Kurmann.InfuseMediaIntegrator.Entities;
 public class MediaFileLibraryOrganizationInfo
 {
     /// <summary>
-    /// Die Mediendatei.
-    /// </summary>
-    public IMediaFileType MediaFileType { get; }
-
-    /// <summary>
     /// Der Dateiname der Zieldatei.
     /// </summary>
     public FileNameInfo TargetFileName { get; }
@@ -26,75 +21,71 @@ public class MediaFileLibraryOrganizationInfo
     /// <summary>
     /// Das Verzeichnis, in das die Datei verschoben werden soll.
     /// </summary>
-    public DirectoryPathInfo TargetSubDirectory { get; }
+    public LibraryFileSubDirectoryPath? TargetSubDirectory { get; }
 
-    private MediaFileLibraryOrganizationInfo(IMediaFileType mediaFileType, FileNameInfo targetFileName, DirectoryPathInfo targetSubDirectory)
+    private MediaFileLibraryOrganizationInfo(FileNameInfo targetFileName, LibraryFileSubDirectoryPath? targetSubDirectory)
     {
-        MediaFileType = mediaFileType;
         TargetFileName = targetFileName;
         TargetSubDirectory = targetSubDirectory;
     }
 
     /// <summary>
-    /// Erstellt ein MediaFileLibraryOrganizationInfo-Objekt.
+    /// Erstellt eine Instanz von MediaFileLibraryOrganizationInfo, wenn die gegebene Mediendatei gültig ist.
+    /// Wenn keine Kategorien-Pfad vorhanden ist, wird der Katgeorien-Pfad aus den Metadaten der Mediendatei gelesen.
+    /// Wenn keine Metadaten vorhanden sind, wird die Mediendatei in das Root-Verzeichnis der Mediathek verschoben.
     /// </summary>
     /// <param name="mediaFile"></param>
     /// <param name="rootDirectory"></param>
     /// <returns></returns>
-    public static Result<MediaFileLibraryOrganizationInfo> Create(IMediaFileType? mediaFile, string rootDirectory)
+    public static Result<MediaFileLibraryOrganizationInfo> Create(IMediaFileType? mediaFile, string? rootDirectory = null)
     {
-        // Prüfe, ob die Mediendatei vorhanden ist
+        // Prüfe, ob die Mediendatei angegeben ist
         if (mediaFile == null)
             return Result.Failure<MediaFileLibraryOrganizationInfo>("Media file is null.");
 
-        // Lies in den Metadaten, ob ein Kategorien-Pfad vorhanden ist, bspw. "Family/2024".
-        // In den Metadaten entspricht das dem Album.
-        var categoryPath = mediaFile.Metadata?.Album;
+        // Ermittle den Ziel-Dateinamen
+        var targetFileName = GetTargetFileName(mediaFile);
+        var targetSubDirectory = GetTargetSubDirectoryFromMetadata(mediaFile).GetValueOrDefault() ?? GetTargetSubDirectoryFromSourcePath(mediaFile, rootDirectory);
 
-        // Unterscheide zwischen Medientypen
-        return mediaFile switch
+        return Result.Success(new MediaFileLibraryOrganizationInfo(targetFileName.Value, targetSubDirectory.Value));
+    }
+
+    private static Result<LibraryFileSubDirectoryPath?> GetTargetSubDirectoryFromSourcePath(IMediaFileType mediaFile, string? directoryPath)
+    {
+        // Wenn kein Kategorien-Pfad angegeben ist, wird der Root-Verzeichnis-Pfad zurückgegeben
+        if (directoryPath == null)
+            return null;
+
+        var path = LibraryFileSubDirectoryPath.Create(mediaFile.FilePath, directoryPath);
+        if (path.IsFailure)
+            return Result.Failure<LibraryFileSubDirectoryPath?>($"Error on creating subdirectory path: {path.Error}");
+
+        return path.Value;
+    }
+
+    private static Result<LibraryFileSubDirectoryPath> GetTargetSubDirectoryFromMetadata(IMediaFileType mediaFile)
+    {
+        // Der Kategorien-Pfad ist der Album-Name
+        var categoryPathFromMetadata = DirectoryPathInfo.Create(mediaFile.Metadata?.Album);
+        if (categoryPathFromMetadata.IsFailure)
+            return Result.Failure<LibraryFileSubDirectoryPath>($"Error on reading category path from metadata: {categoryPathFromMetadata.Error}");
+
+        return LibraryFileSubDirectoryPath.Create(categoryPathFromMetadata.Value);
+    }
+
+    private static Result<FileNameInfo> GetTargetFileName(IMediaFileType mediaFile)
+    {
+        // Wenn die Datei ein Bild ist, wird der Dateiname mit dem Präfix "fanart-" zurückgegeben
+        if (mediaFile is JpegImage)
         {
-            Mpeg4Video mpeg4Video => Create(mpeg4Video, rootDirectory),
-            QuickTimeVideo quickTimeVideo => Create(quickTimeVideo),
-            JpegImage jpegImage => Create(jpegImage),
-            _ => Result.Failure<MediaFileLibraryOrganizationInfo>("Media file type is not supported.")
-        };
-    }
+            var fileName = LibraryFanartImageFileName.CreateWithAddedFanartPrefix(mediaFile.FilePath.FileName);
+            if (fileName.IsFailure)
+                return Result.Failure<FileNameInfo>($"Error on creating fanart file name: {fileName.Error}");
 
-    public static Result<MediaFileLibraryOrganizationInfo> Create(Mpeg4Video mpeg4Video, string rootDirectory)
-    {
-        // Ermittle das Zielformat des Dateinamens
-        var targetFileName = LibraryVideoFileName.Create(mpeg4Video.FilePath.FileName);
-        if (targetFileName.IsFailure)
-            return Result.Failure<MediaFileLibraryOrganizationInfo>($"Error on reading file info: {targetFileName.Error}");
+            return fileName.Value.FileName;
+        }
 
-        // Ermittle das Zielverzeichnis
-        var targetSubDirectory = LibraryFileSubDirectoryPath.Create(mpeg4Video.FilePath.FilePath, rootDirectory);
-        if (targetSubDirectory.IsFailure)
-            return Result.Failure<MediaFileLibraryOrganizationInfo>($"Error on reading file info: {targetSubDirectory.Error}");
-    }
-
-    public static Result<MediaFileLibraryOrganizationInfo> Create(QuickTimeVideo quickTimeVideo, string rootDirectory)
-    {
-        // Ermittle das Zielformat des Dateinamens
-        var targetFileName = LibraryVideoFileName.Create(quickTimeVideo.FilePath.FileName);
-        if (targetFileName.IsFailure)
-            return Result.Failure<MediaFileLibraryOrganizationInfo>($"Error on reading file info: {targetFileName.Error}");
-
-        // Ermittle das Zielverzeichnis
-        var targetSubDirectory = LibraryFileSubDirectoryPath.Create(quickTimeVideo.FilePath.FilePath, rootDirectory);
-        if (targetSubDirectory.IsFailure)
-            return Result.Failure<MediaFileLibraryOrganizationInfo>($"Error on reading file info: {targetSubDirectory.Error}");
-    }
-
-    public static Result<MediaFileLibraryOrganizationInfo> Create(JpegImage jpegImage, string rootDirectory)
-    {
-        // todo: implement art work for jpeg images with "-fanart" in the file name
-        var targetFileName = jpegImage.FilePath.FileName;
-
-        // Ermittle das Zielverzeichnis
-        var targetSubDirectory = LibraryFileSubDirectoryPath.Create(jpegImage.FilePath.FilePath, rootDirectory);
-        if (targetSubDirectory.IsFailure)
-            return Result.Failure<MediaFileLibraryOrganizationInfo>($"Error on reading file info: {targetSubDirectory.Error}");
+        // Wenn die Datei kein Bild ist, wird der Dateiname unverändert zurückgegeben
+        return mediaFile.FilePath.FileName;
     }
 }
