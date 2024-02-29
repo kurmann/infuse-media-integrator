9bb44d30bb1a0fd315ea99644a31d6647af9ff3e
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-02-29 08:09:25 +0000

    add typed create method #18

--- src/ConsoleApp/Entities/MediaFileTypes/MediaFileTypeDetector.cs
+++ src/ConsoleApp/Entities/MediaFileTypes/MediaFileTypeDetector.cs
@@ -18,41 +18,58 @@ public class MediaFileTypeDetector
             if (filePath.IsFailure)
                 return Result.Failure<IMediaFileType>($"Error on reading file info: {filePath.Error}");
 
-            // Lies die Dateiendung
-            var extension = Path.GetExtension(filePath.Value.FilePath).ToLowerInvariant();
-
-            // Gib das passende IMediaFileType-Objekt zurück
-            if (Mpeg4Video.FileExtensions.Contains(extension))
-            {
-                var mpeg4Video = Mpeg4Video.Create(path);
-                if (mpeg4Video.IsFailure)
-                    return Result.Failure<IMediaFileType>($"Error on reading file info: {mpeg4Video.Error}");
-                return mpeg4Video.Value;
-            }
-            if (QuickTimeVideo.FileExtensions.Contains(extension))
-            {
-                var quickTimeVideo = QuickTimeVideo.Create(path);
-                if (quickTimeVideo.IsFailure)
-                    return Result.Failure<IMediaFileType>($"Error on reading file info: {quickTimeVideo.Error}");
-                return quickTimeVideo.Value;
-            }
-            if (JpegImage.FileExtensions.Contains(extension))
-            {
-                var jpegImage = JpegImage.Create(path);
-                if (jpegImage.IsFailure)
-                    return Result.Failure<IMediaFileType>($"Error on reading file info: {jpegImage.Error}");
-                return jpegImage.Value;
-            }
-            var notSupportedFile = NotSupportedFile.Create(path);
-            if (notSupportedFile.IsFailure)
-                return Result.Failure<IMediaFileType>($"Error on reading file info: {notSupportedFile.Error}");
-            return notSupportedFile.Value;
+            return GetMediaFileType(path, filePath);
 
         }
         catch (Exception e)
         {
             return Result.Failure<IMediaFileType>($"Error on reading file info: {e.Message}");
         }
+    }
+
+    public static Result<IMediaFileType> GetMediaFile(FilePathInfo filePath)
+    {
+        try
+        {
+            return GetMediaFileType(filePath.FilePath, filePath);
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<IMediaFileType>($"Error on reading file info: {e.Message}");
+        }
+    }
+
+    private static Result<IMediaFileType> GetMediaFileType(string path, Result<FilePathInfo> filePath)
+    {
+        // Lies die Dateiendung
+        var extension = Path.GetExtension(filePath.Value.FilePath).ToLowerInvariant();
+
+        // Gib das passende IMediaFileType-Objekt zurück
+        if (Mpeg4Video.FileExtensions.Contains(extension))
+        {
+            var mpeg4Video = Mpeg4Video.Create(path);
+            if (mpeg4Video.IsFailure)
+                return Result.Failure<IMediaFileType>($"Error on reading file info: {mpeg4Video.Error}");
+            return mpeg4Video.Value;
+        }
+        if (QuickTimeVideo.FileExtensions.Contains(extension))
+        {
+            var quickTimeVideo = QuickTimeVideo.Create(path);
+            if (quickTimeVideo.IsFailure)
+                return Result.Failure<IMediaFileType>($"Error on reading file info: {quickTimeVideo.Error}");
+            return quickTimeVideo.Value;
+        }
+        if (JpegImage.FileExtensions.Contains(extension))
+        {
+            var jpegImage = JpegImage.Create(path);
+            if (jpegImage.IsFailure)
+                return Result.Failure<IMediaFileType>($"Error on reading file info: {jpegImage.Error}");
+            return jpegImage.Value;
+        }
+        var notSupportedFile = NotSupportedFile.Create(path);
+        if (notSupportedFile.IsFailure)
+            return Result.Failure<IMediaFileType>($"Error on reading file info: {notSupportedFile.Error}");
+        return notSupportedFile.Value;
     }
 }
 
