ec69829c851d0806f962635a87500b70966ddb35
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-02 22:47:13 +0100

    search media library fully working #19

--- src/Tests/Queries/MediaLibraryQueryTest.cs
+++ src/Tests/Queries/MediaLibraryQueryTest.cs
@@ -55,4 +55,70 @@ public class MediaLibraryQueryTests
         Assert.AreEqual("2024-02-16 Krokus Testaufnahme.m4v", result.Value[0].FilePath.FileName);
         Assert.AreEqual("2024-02-16 Krokus Testaufnahme-fanart.jpg", result.Value[1].FilePath.FileName);
     }
+
+    [TestMethod] // Suche nach Titel liefert korrektes Ergebnis
+    public void WithTitle_ShouldReturnCorrectResult_WhenTitleIsSet()
+    {
+        // Arrange
+        var query = new MediaLibraryQuery(MediaLibraryPath).ByProperties().WithTitle("Krokus Testaufnahme").Query();
+
+        // Act
+        var result = query.Execute();
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Value);
+        Assert.AreEqual(2, result.Value.Count);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme.m4v", result.Value[0].FilePath.FileName);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme-fanart.jpg", result.Value[1].FilePath.FileName);
+    }
+
+    [TestMethod] // Suche nach Titel und Aufnahme liefert korrektes Ergebnis
+    public void WithTitleAndRecording_ShouldReturnCorrectResult_WhenTitleAndDateAreSet()
+    {
+        // Arrange
+        var query = new MediaLibraryQuery(MediaLibraryPath).ByProperties().WithTitle("Krokus Testaufnahme").WithDate(new DateOnly(2024, 2, 16)).Query();
+
+        // Act
+        var result = query.Execute();
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Value);
+        Assert.AreEqual(2, result.Value.Count);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme.m4v", result.Value[0].FilePath.FileName);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme-fanart.jpg", result.Value[1].FilePath.FileName);
+    }
+
+    [TestMethod] // Suche nach leerem Titel und leerer Aufnahme liefert leere Ergebnisliste
+    public void WithEmptyTitleAndRecording_ShouldReturnEmptyResult_WhenTitleAndDateAreEmpty()
+    {
+        // Arrange
+        var query = new MediaLibraryQuery(MediaLibraryPath).ByProperties().WithTitle(string.Empty).WithDate(DateOnly.MinValue).Query();
+
+        // Act
+        var result = query.Execute();
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Value);
+        Assert.AreEqual(0, result.Value.Count);
+    }
+
+    [TestMethod] // Suche nach gesetztem Datum und leerem Titel liefert korrektes Ergebnis
+    public void WithRecordingAndEmptyTitle_ShouldReturnCorrectResult_WhenDateIsSetAndTitleIsEmpty()
+    {
+        // Arrange
+        var query = new MediaLibraryQuery(MediaLibraryPath).ByProperties().WithTitle(string.Empty).WithDate(new DateOnly(2024, 2, 16)).Query();
+
+        // Act
+        var result = query.Execute();
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsNotNull(result.Value);
+        Assert.AreEqual(2, result.Value.Count);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme.m4v", result.Value[0].FilePath.FileName);
+        Assert.AreEqual("2024-02-16 Krokus Testaufnahme-fanart.jpg", result.Value[1].FilePath.FileName);
+    }
 }
