72f271ea79b6fdf49b7f70cd778a226a93b017a5
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-02 19:08:46 +0100

    improved fluent api #19

--- src/ConsoleApp/Queries/MediaLibraryQuery.cs
+++ src/ConsoleApp/Queries/MediaLibraryQuery.cs
@@ -1,36 +1,14 @@
-using System.Net;
 using CSharpFunctionalExtensions;
 using Kurmann.InfuseMediaIntegrator.Entities.Elementary;
 using Kurmann.InfuseMediaIntegrator.Entities.MediaFileTypes;
-using Kurmann.InfuseMediaIntegrator.Entities.MediaLibrary;
 
 namespace Kurmann.InfuseMediaIntegrator.Queries;
 
-/// <summary>
-/// Liefert Medien-Dateien aus dem Medienverzeichnis.
-/// </summary>
-/// <param name="mediaLibraryPath"></param>
 public class MediaLibraryQuery(string mediaLibraryPath) : IQueryService<List<IMediaFileType>>
 {
-    public string MediaLibraryPath { get; } = mediaLibraryPath;
-
-    /// <summary>
-    /// Die eindeutige ID des Medienverzeichnisses.
-    /// Besteht aus einem ISO-Datumsformat (YYYY-MM-DD) und einem Titel.
-    /// Schema: {YYYY-MM-DD} {Title}
-    /// Entspricht in den meisten Fällen dem Dateinamen des Hauptmediums (Video) ohne Dateiendung.
-    /// </summary>
-    public string? Id { get; private set; }
-
-    /// <summary>
-    /// Das Aufnahmedatum nach ISO-8601.
-    /// </summary>
-    public string? Date { get; private set; }
-
-    /// <summary>
-    /// Der Titel des Medienverzeichnisses.
-    /// </summary>
-    public string? Title { get; private set; }
+    private string MediaLibraryPath { get; set; } = mediaLibraryPath;
+    private string? Id { get; set; }
+    private MediaLibrarySpecificProperties? SpecificProperties { get; set; }
 
     /// <summary>
     /// Führt die Abfrage aus.
@@ -42,16 +20,21 @@ public class MediaLibraryQuery(string mediaLibraryPath) : IQueryService<List<IMe
         if (string.IsNullOrWhiteSpace(MediaLibraryPath))
             return Result.Failure<List<IMediaFileType>>("Media library path is empty.");
 
-        // Wenn eine ID angegeben ist, suche nach Medien-Dateien im Medienverzeichnis.
-        if (!string.IsNullOrWhiteSpace(Id))
-        {
-            // Prüfe, ob das Verzeichnis gültig ist
+        // Prüfe, ob das Verzeichnis gültig ist
             var mediaLibraryDirectory = DirectoryPathInfo.Create(MediaLibraryPath);
             if (mediaLibraryDirectory.IsFailure)
                 return Result.Failure<List<IMediaFileType>>($"An error occurred while searching the media library: {mediaLibraryDirectory.Error}");
 
+        // Wenn eine ID angegeben ist, suche nach Medien-Dateien im Medienverzeichnis.
+        if (!string.IsNullOrWhiteSpace(Id))
+        {
             // Suche nach Medien-Dateien im Medienverzeichnis.
             return SearchMediaLibrary(mediaLibraryDirectory.Value, Id);
+        }
+
+        // Wenn spezifische Eigenschaften angegeben sind, suche nach Medien-Dateien im Medienverzeichnis.
+        if (SpecificProperties is not null)
+        {
         }
 
         return new Result<List<IMediaFileType>>();
@@ -70,26 +53,16 @@ public class MediaLibraryQuery(string mediaLibraryPath) : IQueryService<List<IMe
         return this;
     }
 
-    /// <summary>
-    /// Setzt den Titel des Mediums nach dem gesucht werden soll.
-    /// </summary>
-    /// <param name="title"></param>
-    /// <returns></returns>
-    public MediaLibraryQuery WithTitle(string title)
+    public MediaLibrarySpecificProperties Specific()
     {
-        Title = title;
-        return this;
+        SpecificProperties = new MediaLibrarySpecificProperties();
+        return SpecificProperties;
     }
 
-    /// <summary>
-    /// Setzt das Aufnahmedatum des Mediums nach dem gesucht werden soll.
-    /// </summary>
-    /// <param name="date"></param>
-    /// <returns></returns>
-    public MediaLibraryQuery WithDate(string date)
+    private static Result<List<IMediaFileType>> SearchMediaLibrary(DirectoryPathInfo? mediaLibraryDirectory, DateOnly dateOnly)
     {
-        Date = date;
-        return this;
+        // Suche nach Medien-Dateien im Medienverzeichnis.
+        return SearchMediaLibrary(mediaLibraryDirectory, dateOnly.ToString());
     }
 
     private static Result<List<IMediaFileType>> SearchMediaLibrary(DirectoryPathInfo? mediaLibraryDirectory, string? id)
@@ -103,7 +76,7 @@ public class MediaLibraryQuery(string mediaLibraryPath) : IQueryService<List<IMe
             return new List<IMediaFileType>();
 
         // Suche nach Medien-Dateien im Medienverzeichnis.
-        try 
+        try
         {
             var iMediaFiles = SearchFiles(mediaLibraryDirectory, id);
 
@@ -168,5 +141,31 @@ public class MediaLibraryQuery(string mediaLibraryPath) : IQueryService<List<IMe
                 directories.Enqueue(subdir);
             }
         }
+    }
+}
+
+public class MediaLibrarySpecificProperties
+{
+    private string? Title { get; set; }
+    private DateOnly? Date { get; set; }
+
+    /// <summary>
+    /// Setzt den Titel des Mediums nach dem gesucht werden soll.
+    /// </summary>
+    /// <param name="title"></param>
+    /// <returns></returns>
+    public void WithTitle(string title)
+    {
+        Title = title;
+    }
+
+    /// <summary>
+    /// Setzt das Aufnahmedatum des Mediums nach dem gesucht werden soll.
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public void WithDate(DateOnly date)
+    {
+        Date = date;
     }
 }
