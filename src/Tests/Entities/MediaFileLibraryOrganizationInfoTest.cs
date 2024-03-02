7164bac997b69263c0f9ace85dba48a2cefe2db4
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-02 14:21:30 +0100

    additional tests #14

--- src/Tests/Entities/MediaFileLibraryOrganizationInfoTest.cs
+++ src/Tests/Entities/MediaFileLibraryOrganizationInfoTest.cs
@@ -43,4 +43,58 @@ public class MediaFileLibraryOrganizationInfoTests
         Assert.AreEqual(SubdirectoryDerivationMode.SourcePath, result.Value.SubdirectoryDerivationMode); // Albumname enthält Sonderzeichen, deshalb wird der SourcePath verwendet
         Assert.AreEqual("/", result.Value.TargetSubDirectory?.ToString());
     }
+
+    [TestMethod] // Teste ob Album-Namen mit Schrägstrichen korrekt als Verzeichnis-Trennzeichen interpretiert werden
+    public void Create_WithAlbumTagWithSlashes_ReturnsCorrectSubDirectory()
+    {
+        // Arrange
+        var metadata = MediaFileMetadata.Create("Testvideo").Value.WithAlbum("Familie Kurmann/2021");
+        var path = Path.Combine(InputDirectoryPath, "Testvideo.m4v");
+        var mediaFile = Mpeg4Video.Create(path, metadata).Value;
+
+        // Act
+        var result = MediaFileLibraryOrganizationInfo.Create(mediaFile, InputDirectoryPath);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.AreEqual("Familie Kurmann/2021", result.Value.TargetSubDirectory?.ToString());
+        Assert.IsTrue(result.Value.HasAlbumNameInMetadata);
+        Assert.AreEqual(SubdirectoryDerivationMode.Metadata, result.Value.SubdirectoryDerivationMode);
+    }
+
+    [TestMethod] // Teste ob leere Album-Tags eine Ableitung des Zielverzeichnisses aus dem Quellverzeichnis erzwingen
+    public void Create_WithEmptyAlbumTag_ReturnsCorrectTagetPath()
+    {
+        // Arrange
+        var metadata = MediaFileMetadata.Create("Testvideo").Value.WithAlbum(string.Empty);
+        var path = Path.Combine(InputDirectoryPath, "Testvideo.m4v");
+        var mediaFile = Mpeg4Video.Create(path, metadata).Value;
+
+        // Act
+        var result = MediaFileLibraryOrganizationInfo.Create(mediaFile, InputDirectoryPath);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsTrue(result.Value.HasAlbumNameInMetadata); // Albumname ist vorhanden
+        Assert.AreEqual(SubdirectoryDerivationMode.SourcePath, result.Value.SubdirectoryDerivationMode); // Albumname ist leer, deshalb wird der SourcePath verwendet
+        Assert.AreEqual("/", result.Value.TargetSubDirectory?.ToString());
+    }
+
+    [TestMethod] // Teste ob bei fehlendem Album-Tag das Zielverzeichnis aus dem Quellverzeichnis abgeleitet wird
+    public void Create_WithoutAlbumTag_ReturnsCorrectSubDirectory()
+    {
+        // Arrange
+        var metadata = MediaFileMetadata.Create("Testvideo").Value;
+        var path = Path.Combine(InputDirectoryPath, "New Project/Testvideo.m4v");
+        var mediaFile = Mpeg4Video.Create(path, metadata).Value;
+
+        // Act
+        var result = MediaFileLibraryOrganizationInfo.Create(mediaFile, InputDirectoryPath);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccess);
+        Assert.IsFalse(result.Value.HasAlbumNameInMetadata);
+        Assert.AreEqual(SubdirectoryDerivationMode.SourcePath, result.Value.SubdirectoryDerivationMode);
+        Assert.AreEqual("/New Project", result.Value.TargetSubDirectory?.ToString());
+    }
 }
