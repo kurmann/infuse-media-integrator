1f65b93aa9e14501cdb32fd6cf9dbf73163db5b3
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-16 22:02:50 +0100

    added unsubscribe method, detailed implementation doc #23

--- src/ConsoleApp/Services/MessageService.cs
+++ src/ConsoleApp/Services/MessageService.cs
@@ -20,58 +20,88 @@ public interface IMessageService
     void Subscribe<TMessage>(Action<TMessage> handler) where TMessage : IEventMessage;
 }
 
+/// <summary>
+/// Verantwortlich für das Senden und Empfangen von Nachrichten.
+/// </summary>
+/// <remarks>
+/// Dieser Service ist ein einfacher Event-Bus, der Nachrichten an registrierte Handler sendet.
+/// </remarks>
+/// <seealso cref="IMessageService" />
+/// <seealso cref="IEventMessage" />
+/// <seealso cref="EventMessageBase" />
+/// <seealso cref="IEventMessage" />
+//
+// Einige wichtige Punkte zur Implementierung:
+//
+// Thread-Sicherheit: Durch die Verwendung von lock beim Aktualisieren der Handler-Listen wird sichergestellt, dass die Operationen 
+// threadsicher sind. Dies verhindert, dass zwei Threads gleichzeitig die Liste der Handler modifizieren.
+// Verwendung von ConcurrentDictionary: Die Verwendung von ConcurrentDictionary anstelle von Dictionary stellt sicher, dass das
+//
+// Hinzufügen und Entfernen von Einträgen threadsicher ist.
+// Verwendung von ToList() bei Iteration: Durch das Kopieren der Liste vor der Iteration wird sichergestellt, dass die Iteration
+// threadsicher ist. Dies verhindert, dass ein anderer Thread die Liste während der Iteration modifiziert.
+//
+// Performance: Die Verwendung von lock kann zu einem Engpass führen, wenn viele Threads versuchen, gleichzeitig zu abonnieren oder zu 
+// deabonnieren. Dies ist in der Regel in Hochlast-Szenarien relevant. In den meisten Anwendungsfällen sollte dies jedoch kein Problem darstellen.
+//
+// Liste kopieren vor der Modifikation: Durch das Kopieren der Liste vor der Modifikation und das Ersetzen der alten Liste im Dictionary 
+// wird die Unveränderlichkeit der Listen für außenstehende Betrachter sichergestellt. Das bedeutet, dass keine Änderungen an der 
+// Liste vorgenommen werden, während ein anderer Thread sie möglicherweise durchläuft.
 public class MessageService : IMessageService
 {
-    // Eine threadsichere Sammlung zum Speichern von Nachrichten-Handlern.
-    // 'ConcurrentDictionary' wird aufgrund seiner Thread-Sicherheit und seiner effizienten, lock-freien Lesevorgänge verwendet.
     private readonly ConcurrentDictionary<Type, List<Delegate>> _handlers = new();
 
-    /// <summary>
-    /// Sendet eine Nachricht vom Typ 'TMessage' an alle Abonnenten.
-    /// </summary>
-    /// <typeparam name="TMessage"></typeparam>
-    /// <param name="message"></param>
     public void Send<TMessage>(TMessage message) where TMessage : IEventMessage
     {
-        // Wenn Abonnenten für den Nachrichtentyp vorhanden sind, rufe deren Handler auf.
         if (_handlers.TryGetValue(typeof(TMessage), out var subscribers))
         {
-            foreach (var handler in subscribers)
+            foreach (var handler in subscribers.ToList()) // ToList() für Thread-Sicherheit bei Iteration
             {
-                // Rufe den Handler dynamisch mit der Nachricht auf.
                 handler.DynamicInvoke(message);
             }
         }
     }
 
-    /// <summary>
-    /// Abonniert eine Nachricht vom Typ 'TMessage' mit einem Handler.
-    /// </summary>
-    /// <typeparam name="TMessage"></typeparam>
-    /// <param name="handler"></param>
     public void Subscribe<TMessage>(Action<TMessage> handler) where TMessage : IEventMessage
     {
         var key = typeof(TMessage);
-        var newHandlersList = new List<Delegate>();
-
-        // Diese Umsetzung stellt sicher, dass das Hinzufügen von Handlern threadsicher ist, indem es eine neue Liste von 
-        // Handlern erstellt und diese dann ersetzt, anstatt die bestehende Liste direkt zu ändern.
         _handlers.AddOrUpdate(key,
-
-            // Hinzufügen, wenn der Key noch nicht existiert
-            addValueFactory: _ => [handler],
-            
-            // Update der vorhandenen Liste, wenn der Key existiert
-            updateValueFactory: (type, existingHandlers) =>
+            _ => [handler],
+            (_, existingHandlers) =>
             {
-                newHandlersList = new List<Delegate>(existingHandlers)
+                lock (existingHandlers)
                 {
-                    handler
-                };
-
-                return newHandlersList;
+                    var newHandlersList = new List<Delegate>(existingHandlers) { handler };
+                    return newHandlersList;
+                }
             }
         );
     }
 
+    public void Unsubscribe<TMessage>(Action<TMessage> handler) where TMessage : IEventMessage
+    {
+        var key = typeof(TMessage);
+        if (_handlers.TryGetValue(key, out var subscribers))
+        {
+            lock (subscribers)
+            {
+                if (subscribers.Contains(handler))
+                {
+                    var newSubscribersList = new List<Delegate>(subscribers);
+                    newSubscribersList.Remove(handler);
+                    
+                    // Ersetze die alte Liste mit der neuen, aktualisierten Liste
+                    if (newSubscribersList.Count > 0)
+                    {
+                        _handlers[key] = newSubscribersList;
+                    }
+                    else
+                    {
+                        // Wenn keine Subscriber mehr vorhanden sind, entferne den Eintrag aus dem Dictionary
+                        ((ICollection<KeyValuePair<Type, List<Delegate>>>)_handlers).Remove(new KeyValuePair<Type, List<Delegate>>(key, subscribers));
+                    }
+                }
+            }
+        }
+    }
 }
