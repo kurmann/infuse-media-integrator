d6aa2b476f0e4cf9e9695899c3f084f4f1bb091e
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-16 16:49:01 +0100

    completed messaging service #23

--- src/ConsoleApp/Services/MessageService.cs
+++ src/ConsoleApp/Services/MessageService.cs
@@ -4,28 +4,47 @@ namespace Kurmann.InfuseMediaIntegrator.Services;
 
 public interface IMessageService
 {
-    void Send<TNachricht>(TNachricht nachricht);
-    void Subscribe<TNachricht>(Action<TNachricht> handler);
+    // Sendet eine Nachricht eines beliebigen Typs.
+    void Send<TMessage>(TMessage message);
+    
+    // Abonniert eine Nachricht eines beliebigen Typs mit einem Handler.
+    void Subscribe<TMessage>(Action<TMessage> handler);
 }
 
 public class MessageService : IMessageService
 {
+    // Eine threadsichere Sammlung zum Speichern von Nachrichten-Handlern.
+    // 'ConcurrentDictionary' wird aufgrund seiner Thread-Sicherheit und seiner effizienten, lock-freien Lesevorgänge verwendet.
     private readonly ConcurrentDictionary<Type, List<Delegate>> _handlers = new();
 
-    public void Send<TMessage>(TMessage nachricht)
+    /// <summary>
+    /// Sendet eine Nachricht vom Typ 'TMessage' an alle Abonnenten.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="message"></param>
+    public void Send<TMessage>(TMessage message)
     {
+        // Wenn Abonnenten für den Nachrichtentyp vorhanden sind, rufe deren Handler auf.
         if (_handlers.TryGetValue(typeof(TMessage), out var subscribers))
         {
             foreach (var handler in subscribers)
             {
-                handler.DynamicInvoke(nachricht);
+                // Rufe den Handler dynamisch mit der Nachricht auf.
+                handler.DynamicInvoke(message);
             }
         }
     }
 
-    public void Subscribe<TNachricht>(Action<TNachricht> handler)
+    /// <summary>
+    /// Abonniert eine Nachricht vom Typ 'TMessage' mit einem Handler.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="handler"></param>
+    public void Subscribe<TMessage>(Action<TMessage> handler)
     {
-        var subscribers = _handlers.GetOrAdd(typeof(TNachricht), _ => new List<Delegate>());
+        // Fügt auf threadsichere Weise einen Nachrichten-Handler für den angegebenen Nachrichtentyp hinzu.
+        // Die Operationen 'GetOrAdd' und 'Add' sind atomar und verhindern race conditions.
+        var subscribers = _handlers.GetOrAdd(typeof(TMessage), _ => []);
         subscribers.Add(handler);
     }
 }
