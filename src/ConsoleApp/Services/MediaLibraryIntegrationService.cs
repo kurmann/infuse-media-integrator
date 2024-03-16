37d7eb44d15e7674d0c7f045a754be2158dfbe50
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-03-16 18:23:21 +0100

    Add MediaLibraryIntegrationService class

---
+++ src/ConsoleApp/Services/MediaLibraryIntegrationService.cs
@@ -0,0 +1,27 @@
+using Kurmann.InfuseMediaIntegrator.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Kurmann.InfuseMediaIntegrator.Services;
+
+public class MediaLibraryIntegrationService
+{
+    private readonly ILogger<MediaLibraryIntegrationService> _logger;
+    private readonly IMessageService _messageService;
+
+    public MediaLibraryIntegrationService(ILogger<MediaLibraryIntegrationService> logger, IMessageService messageService)
+    {
+        _logger = logger;
+        _messageService = messageService;
+
+        // Abonnieren von Nachrichten, um Dateien zu verschieben, wenn die entsprechende Nachricht empfangen wird.
+        _messageService.Subscribe<FileAddedEventMessage>(MoveFileToLibrary);
+    }
+
+    private void MoveFileToLibrary(FileAddedEventMessage fileCreatedEvent)
+    {
+        // Implementiere hier die Logik zum Verschieben der Datei.
+        // Verwende den Inhalt von 'MoveFileToMediaLibraryCommand' als Referenz für die Implementierung.
+
+    }
+
+}
